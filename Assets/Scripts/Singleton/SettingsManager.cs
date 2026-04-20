a0d0df90abca57895e1902e805af7c52ed54bedf
Facundo947 <facundoarosmino@gmail.com>
2026-04-20 14:11:07 -0300

    Cambios Singleton

--- Assets/Scripts/Singleton/SettingsManager.cs
+++ Assets/Scripts/Singleton/SettingsManager.cs
@@ -2,15 +2,51 @@ using UnityEngine;
 
 public class SettingsManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static SettingsManager Instance { get; private set; }
+
+    [SerializeField] private float musicVolume = 1f;
+    [SerializeField] private float sfxVolume = 1f;
+
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Debug.Log("[SettingsManager] Instancia duplicada destruida.");
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetMusicVolume(float volume)
     {
-        
+        musicVolume = volume;
+
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.SetMusicVolume(volume);
+        }
+    }
+
+    public void SetSfxVolume(float volume)
+    {
+        sfxVolume = volume;
+
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.SetSfxVolume(volume);
+        }
+    }
+
+    public float GetMusicVolume()
+    {
+        return musicVolume;
+    }
+
+    public float GetSfxVolume()
+    {
+        return sfxVolume;
     }
 }
